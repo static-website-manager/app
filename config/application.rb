b8cbf13d84cce02af53a5cdcb3175a946c512bc0
Theodore Kimble <mail@theodorekimble.com>
2015-06-20 09:34:38 -0700

    Add configuration to skip assets and helpers autogeneration

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module StaticWebsiteManager
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Do not automatically generate assets or helpers.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
