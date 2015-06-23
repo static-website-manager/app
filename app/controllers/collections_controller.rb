eba138321a1c52639c4d760cef09abc00d46af5c
Theodore Kimble <mail@theodorekimble.com>
2015-06-22 18:09:28 -0700

    Add collections controller

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,15 @@
+class CollectionsController < ApplicationController
+  before_action :require_user
+
+  before_action do
+    @website = current_user.websites.find(params[:website_id])
+    @branch = @website.repository.branches.find do |branch|
+      branch.refname == params[:branch_id]
+    end
+
+    raise ActiveRecord::RecordNotFound unless @branch
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :websites, only: %i[index show] do
     resource :setup, only: %i[new create], path_names: { new: '' }
     resources :branches, only: %i[show], path: '' do
+      resources :collections, only: %i[index]
       resources :pages, only: %i[index]
       resources :posts, only: %i[index]
     end
