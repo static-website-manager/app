0e71031253c159bbd5f0e5c23357ea5417e60645
Theodore Kimble <mail@theodorekimble.com>
2015-08-05 15:18:05 -0700

    Add drafts tab and update draft edit view

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :authorizations, only: %i[index new create edit update destroy], path: 'team'
     resources :branches, only: %i[show], path: '' do
       resources :commits, only: %i[index], path: 'history'
-      resources :drafts, only: %i[edit update]
+      resources :drafts, only: %i[index edit update]
       resources :pages, only: %i[index edit update]
       resources :posts, only: %i[index edit update]
     end
