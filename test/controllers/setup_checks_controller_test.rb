cd27fdd792da8dbdb7998e9b5f01b1e3b3970157
Theodore Kimble <mail@theodorekimble.com>
2015-08-14 15:16:34 -0700

    Remove controller tests

--- test/controllers/setup_checks_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SetupChecksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/setups_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SetupsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
