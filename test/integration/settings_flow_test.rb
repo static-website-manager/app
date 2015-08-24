f137b3a5d7342eb6160a63fe2e6e55371c7841d0
Theodore Kimble <mail@theodorekimble.com>
2015-08-23 20:10:11 -0700

    Simple naming of settings and setups tests

--- test/integration/settings_flow_test.rb
+++ test/integration/settings_flow_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-class WebsiteSettingsFlowTest < ActionDispatch::IntegrationTest
-  def test_website_settings_as_new_website
+class SettingsFlowTest < ActionDispatch::IntegrationTest
+  def test_settings_as_new_website
     sign_in users(:user_one)
     get_via_redirect edit_website_settings_path(websites(:new_website))
     assert_response 200
@@ -12,11 +12,11 @@ class WebsiteSettingsFlowTest < ActionDispatch::IntegrationTest
     refute_equal 'Test', websites(:new_website).reload.name
   end
 
-  def test_website_settings
+  def test_settings
     visit_settings_page(assert: true)
   end
 
-  def test_failed_website_settings_update
+  def test_failed_settings_update
     visit_settings_page
     patch_via_redirect edit_website_settings_path(websites(:sample_website)), invalid_params
     assert_response 422
@@ -24,7 +24,7 @@ class WebsiteSettingsFlowTest < ActionDispatch::IntegrationTest
     refute_equal '', websites(:sample_website).reload.name
   end
 
-  def test_successfull_website_settings_update
+  def test_successfull_settings_update
     visit_settings_page
     patch_via_redirect edit_website_settings_path(websites(:sample_website)), valid_params
     assert_response 200
--- test/integration/setups_flow_test.rb
+++ test/integration/setups_flow_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
-class WebsiteSetupsFlowTest < ActionDispatch::IntegrationTest
-  def test_website_setup
-    visit_website_setup_page(websites(:new_website))
+class SetupsFlowTest < ActionDispatch::IntegrationTest
+  def test_setup
+    visit_setup_page(websites(:new_website))
   end
 
-  def test_website_setup_and_wait_for_repository
-    # visit_website_setup_page(websites(:new_website), assert: false)
+  def test_setup_and_wait_for_repository
+    # visit_setup_page(websites(:new_website), assert: false)
     # wait and push to master and follow redirect
     # assert_response 200
     # assert_equal website_branch_path(websites(:new_website), websites(:new_website).branch(users(:user_one))), path
   end
 
-  def test_website_setup_as_sample_website
-    visit_website_setup_page(websites(:sample_website), assert: false)
+  def test_setup_as_sample_website
+    visit_setup_page(websites(:sample_website), assert: false)
     assert_response 200
     assert_equal website_branch_path(websites(:sample_website), websites(:sample_website).branch(users(:user_one))), path
   end
 
   private
 
-  def visit_website_setup_page(website, assert: false)
+  def visit_setup_page(website, assert: false)
     sign_in users(:user_one)
     get_via_redirect new_website_setup_path(website)
 
