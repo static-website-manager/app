d69f22f5f387d0d0b5bce57c38599dacb23e8ca6
Theodore Kimble <mail@theodorekimble.com>
2015-08-25 16:10:18 -0700

    Add commits flow integration test

---
+++ test/integration/commits_flow_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CommitsFlowTest < ActionDispatch::IntegrationTest
+  def test_commits
+    sign_in user
+    get website_branch_commits_path(website, website.branch(user))
+    assert_response 200
+  end
+end
