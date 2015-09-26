01213e20282956faf35c766d76e4cd7d46b791ba
Theodore Kimble <mail@theodorekimble.com>
2015-09-26 13:55:30 -0700

    Complete test mailers and previews

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,5 +1,17 @@
 class UserMailerPreview < ActionMailer::Preview
-  def test_subscription_confirmation
+  def email_confirmation
+    UserMailer.email_confirmation(User.first)
+  end
+
+  def password_reset
+    UserMailer.password_reset(User.first)
+  end
+
+  def password_update_confirmation
+    UserMailer.password_update_confirmation(User.first)
+  end
+
+  def subscription_confirmation
     UserMailer.subscription_confirmation(User.first)
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,6 +1,30 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+  def test_email_confirmation
+    user = User.first
+    email = UserMailer.email_confirmation(user).deliver_now
+    refute ActionMailer::Base.deliveries.empty?
+    assert_equal ['support@staticwebsitemanager.test'], email.from
+    assert_equal [user.email], email.to
+  end
+
+  def test_password_reset
+    user = User.first
+    email = UserMailer.password_reset(user).deliver_now
+    refute ActionMailer::Base.deliveries.empty?
+    assert_equal ['support@staticwebsitemanager.test'], email.from
+    assert_equal [user.email], email.to
+  end
+
+  def test_password_update_confirmation
+    user = User.first
+    email = UserMailer.password_update_confirmation(user).deliver_now
+    refute ActionMailer::Base.deliveries.empty?
+    assert_equal ['support@staticwebsitemanager.test'], email.from
+    assert_equal [user.email], email.to
+  end
+
   def test_subscription_confirmation
     user = User.first
     email = UserMailer.subscription_confirmation(user).deliver_now
