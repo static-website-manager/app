041ecede20f356456f3a74b6441deaef232c1e93
Theodore Kimble <mail@theodorekimble.com>
2015-09-26 13:35:40 -0700

    Remove Form model dependency

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,52 @@
-class Subscription < Form
-  models :user, :website, :authorization
+class Subscription
+  include ActiveModel::Model
 
-  def setup_authorization(authorization)
-    authorization.user = user
-    authorization.website = website
+  attr_accessor :authorization, :user, :website
+
+  def initialize(*args)
+    @authorization = Authorization.new
+    @user = User.new
+    @website = Website.new
+    super
+  end
+
+  def authorization_attributes=(*args)
+    @authorization.assign_attributes(*args)
+  end
+
+  def user_attributes=(*args)
+    @user.assign_attributes(*args)
+  end
+
+  def website_attributes=(*args)
+    @website.assign_attributes(*args)
+  end
+
+  def save
+    ActiveRecord::Base.transaction do
+      @user.save
+      @website.save
+      @authorization.user = @user
+      @authorization.website = @website
+      @authorization.save
+
+      @authorization.errors.each do |attribute, error|
+        errors.add attribute, error
+      end
+
+      @user.errors.each do |attribute, error|
+        errors.add attribute, error
+      end
+
+      @website.errors.each do |attribute, error|
+        errors.add attribute, error
+      end
+
+      if errors.any?
+        raise ActiveRecord::Rollback
+      else
+        true
+      end
+    end
   end
 end
