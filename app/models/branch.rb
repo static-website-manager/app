ac2ce6b8c5af0bd3b041cdaab5fd6acb63e316f8
Theodore Kimble <mail@theodorekimble.com>
2015-09-27 13:15:30 -0700

    Make branch model ActiveModel compliant

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,31 +1,42 @@
 class Branch
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
+  include ActiveModel::Model
 
   def self.find(rugged_repository, name_or_user)
-    raise ActiveRecord::RecordNotFound if name_or_user.blank?
-    raw_name = name_or_user.is_a?(User) ? "swm_user_#{name_or_user.id}" : name_or_user.to_s
-    rugged_branch = rugged_repository.branches[raw_name]
-    if rugged_branch
-      new(rugged_repository, rugged_branch)
+    if name_or_user.blank?
+      raise ActiveRecord::RecordNotFound
     elsif name_or_user.is_a?(User)
-      new(rugged_repository, rugged_repository.branches.create("swm_user_#{name_or_user.id}", 'master'))
+      name = "swm_user_#{name_or_user.id}"
+    else
+      name = name_or_user.to_s
+    end
+
+    rugged_branch = rugged_repository.branches[name]
+
+    if rugged_branch
+      new(
+        name: name,
+        rugged_repository: rugged_repository,
+        rugged_branch: rugged_branch,
+      )
+    elsif name_or_user.is_a?(User)
+      new(
+        name: name,
+        rugged_repository: rugged_repository,
+        rugged_branch: rugged_repository.branches.create(name, 'master'),
+      )
     else
       raise ActiveRecord::RecordNotFound
     end
   end
 
-  def initialize(rugged_repository, rugged_branch)
-    @rugged_repository = rugged_repository
-    @rugged_branch = rugged_branch
-  end
+  attr_accessor :name, :rugged_branch, :rugged_repository
 
   def short_id
-    @rugged_branch.target.oid[0..6]
+    rugged_branch.target.oid[0..6]
   end
 
   def raw_name
-    @rugged_branch.name
+    rugged_branch.name
   end
 
   def name
@@ -76,11 +87,11 @@ class Branch
   end
 
   def target
-    @rugged_branch.target
+    rugged_branch.target
   end
 
   def tree
-    @rugged_branch.target.tree
+    rugged_branch.target.tree
   end
 
   def to_param
