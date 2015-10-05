e1a5d7ee573667ad83be9dadb56a11a3cf9c3713
Theodore Kimble <mail@theodorekimble.com>
2015-10-05 16:22:39 -0700

    Add blob #destroy action

--- app/models/concerns/blob_concern.rb
+++ app/models/concerns/blob_concern.rb
@@ -39,7 +39,48 @@ module BlobConcern
   end
 
   def destroy(branch_name, author_email, author_name, commit_message)
-    false
+    clone_path = Rails.root.join('tmp', "clone_#{rand(1000)}_#{Time.now.to_i}")
+
+    FileUtils.rm_rf(clone_path)
+    FileUtils.mkdir(clone_path)
+
+    begin
+      cloned_repository = Rugged::Repository.clone_at(rugged_repository.path.to_s, clone_path.to_s)
+      cloned_repository.checkout('origin/' + branch_name)
+      cloned_branch = cloned_repository.branches['origin/' + branch_name]
+
+      cloned_index = cloned_repository.index
+      cloned_index.read_tree(cloned_branch.target.tree)
+      cloned_index.remove(full_pathname)
+
+      author = {
+        email: author_email,
+        name: author_name,
+        time: Time.now,
+      }
+
+      Rugged::Commit.create(cloned_repository,
+        author: author,
+        committer: author,
+        message: commit_message.present? ? commit_message : "Remove #{self.class.name}",
+        parents: [cloned_branch.target],
+        tree: cloned_index.write_tree(cloned_repository),
+        update_ref: 'refs/heads/' + branch_name,
+      )
+
+      # rugged_repository.push('origin', [branch_name])
+
+      `
+        cd #{clone_path};
+        git push origin #{branch_name};
+      `
+      true
+    rescue
+      # Cleanup git object?
+      false
+    ensure
+      FileUtils.rm_rf(clone_path)
+    end
   end
 
   def extension
