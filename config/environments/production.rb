72cbcc3b84afe502b7d4b675777fa81a40377422
Theodore Kimble <mail@theodorekimble.com>
2015-11-04 14:11:35 -0800

    Configure production mem_cached_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, *ENV['MEMCACHED_HOSTS'].to_s.split(',')
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
