21c3cf3616efd57e227ed9bb0bdfd91089fc19ce
Theodore Kimble <mail@theodorekimble.com>
2015-11-04 12:34:41 -0800

    Add Sidekiq to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Set Sidekiq as the ActiveJob queue adapter.
+  config.active_job.queue_adapter = :sidekiq
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
