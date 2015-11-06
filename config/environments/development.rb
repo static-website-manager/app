9bd628202726b964bf64f78f6d11c9a0a4730dba
Theodore Kimble <mail@theodorekimble.com>
2015-11-06 09:32:11 -0800

    Log dev env to stdout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,6 @@
+# Ensure Docker receives stdout.
+$stdout.sync = true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -41,4 +44,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Log to stdout for Docker to capture.
+  config.logger = Logger.new(STDOUT)
 end
