894973484a0906f3212743fc14d1911e99a0157a
Theodore Kimble <mail@theodorekimble.com>
2015-11-10 16:01:18 -0800

    Update puma configuration for docker

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,7 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads Integer(ENV['MIN_THREADS'] || 2), Integer(ENV['MAX_THREADS'] || 5)
+workers 1
+threads 8,32
 preload_app!
 rackup DefaultRackup
-port 3000
 environment ENV['RAILS_ENV'] || 'development'
 
 on_worker_boot do
