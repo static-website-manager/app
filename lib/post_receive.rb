bb7e52122a935cb597b8160f7d977ca71f9566c0
Theodore Kimble <mail@theodorekimble.com>
2015-11-20 11:56:16 -0800

    Use hooks app server

--- lib/post_receive.rb
+++ lib/post_receive.rb
@@ -8,7 +8,7 @@ website_id = 'WEBSITE_ID'
 while (input = STDIN.read) != ''
   full_ref = input.split(' ').last
   branch_name = full_ref.sub(/\Arefs\/heads\//, '')
-  uri = URI('http://app:3000/git_post_receive')
+  uri = URI('http://hooks:3000/git_post_receive')
   response = Net::HTTP.post_form(uri, token: 'GIT_HOOK_TOKEN', website_id: website_id, branch_name: branch_name)
   puts response.body
 end
