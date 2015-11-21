56b471feb0a5b456d824b336516daaf231881dff
Theodore Kimble <mail@theodorekimble.com>
2015-11-20 22:59:02 -0800

    Use inline cacert file

--- config/initializers/net_http.rb
+++ config/initializers/net_http.rb
@@ -5,7 +5,7 @@ module Net
     alias_method :original_use_ssl=, :use_ssl=
 
     def use_ssl=(flag)
-      self.ca_file = '/usr/lib/ssl/certs/cacert.pem'
+      self.ca_file = '/app/cacert.pem'
       self.verify_mode = OpenSSL::SSL::VERIFY_PEER
       self.original_use_ssl = flag
     end
