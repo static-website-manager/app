e0212a1156aeb7f0a6ec7bf3a176430080800031
Theodore Kimble <mail@theodorekimble.com>
2015-11-24 00:14:15 -0800

    Remove auto deploy settings

--- db/migrate/002_create_websites.rb
+++ db/migrate/002_create_websites.rb
@@ -7,8 +7,6 @@ class CreateWebsites < ActiveRecord::Migration
       t.integer :subscription_plan, null: false
       t.integer :subscription_status, null: false
       t.boolean :yearly_billing, default: false, null: false
-      t.boolean :auto_create_production_deployment, default: true, null: false
-      t.boolean :auto_create_staging_deployment, default: true, null: false
       t.boolean :auto_rebase_staging_on_production_changes, default: true, null: false
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -90,8 +90,6 @@ ActiveRecord::Schema.define(version: 6) do
     t.integer  "subscription_plan",                                         null: false
     t.integer  "subscription_status",                                       null: false
     t.boolean  "yearly_billing",                            default: false, null: false
-    t.boolean  "auto_create_production_deployment",         default: true,  null: false
-    t.boolean  "auto_create_staging_deployment",            default: true,  null: false
     t.boolean  "auto_rebase_staging_on_production_changes", default: true,  null: false
     t.datetime "created_at",                                                null: false
     t.datetime "updated_at",                                                null: false
